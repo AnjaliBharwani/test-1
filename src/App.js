3a64c0cd30eafe5c1a6a40e263df62935378655c
AnjaliBharwani <acksacks2405@gmail.com>
2023-12-15 23:44:56 +0530

    1st

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>happy coding</p>
+        1st deployed
         <a
           className="App-link"
           href="https://reactjs.org"
